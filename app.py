b5cf3086dd2a2adb5a22c0b5eca433cde8ad7384
Hamed12142212 <155337272+Hamed12142212@users.noreply.github.com>
2024-11-15 01:40:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_cors import CORS
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 app = Flask(__name__)
 
 # Enable CORS for the frontend and allow credentials (cookies)
@@ -85,4 +85,4 @@ def logout():
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000))))
