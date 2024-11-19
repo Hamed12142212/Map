d93c9fd064a9bdeff98f0bce5524fc4471ef7892
Hamed12142212 <155337272+Hamed12142212@users.noreply.github.com>
2024-11-19 21:09:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class User(db.Model):
     __tablename__ = 'users'  # Explicitly setting the table name to 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)  # Password will hold the hashed password
+    password = db.Column(db.String(255), nullable=False)  # Password will hold the hashed password
 
     def __repr__(self):
         return f"<User {self.username}>"
